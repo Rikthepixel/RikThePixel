541e7fd28d7f02642046d9533be0327470927b95
RikThePixel <Rikdenbreejen@hotmail.nl>
2022-03-02 14:28:15 +0100

    [Changes] Make ready for deployment

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>
+      Sup x2
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
