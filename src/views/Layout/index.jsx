bda1af98a3b5fee9f4ba861304d248c2e582fc00
RikThePixel <Rikdenbreejen@hotmail.nl>
2022-04-25 21:40:39 +0200

    [Change] Layout JS looks fancy :3

--- src/views/Layout/index.jsx
+++ src/views/Layout/index.jsx
@@ -2,36 +2,35 @@ import React, { useEffect, useRef } from 'react';
 import { NavLink, Outlet } from "react-router-dom";
 
 const PageLink = ({ className, ...props }) => {
-    return <NavLink className="bg-secondary hover:bg-dark hover:active:bg-light hover:active:text-zinc-600 current-page:bg-active text-center p-2 rounded-lg w-full md:basis-full shadow-lg" {...props} />;
+    return <NavLink className="bg-secondary hover:bg-active hover:active:bg-light hover:active:text-zinc-600 current-page:bg-dark text-center p-2 rounded-lg w-full md:basis-full shadow-lg" {...props} />;
 };
 
 const Layout = () => {
     const outletContainerRef = useRef();
     const navbarContainerRef = useRef();
     useEffect(() => {
-        if (!navbarContainerRef.current || !outletContainerRef.current) return null;
+        const navBar = navbarContainerRef.current;
+        const outlet = outletContainerRef.current;
+        if (!navBar || !outlet) return null;
 
-        outletContainerRef.current.style.paddingBottom = navbarContainerRef.current.offsetHeight + "px";
-        const navObserver = new ResizeObserver(
-            (entries) => outletContainerRef.current.style.paddingBottom = entries[0].target.offsetHeight + "px"
-        );
-        navObserver.observe(navbarContainerRef.current);
-
-        const outletObserver = new ResizeObserver((entries) => {
+        outlet.style.paddingBottom = navBar.offsetHeight + "px";
+        const navObserver = new ResizeObserver(entries => { outlet.style.paddingBottom = entries[0].target.offsetHeight + "px"; });
+        const outletObserver = new ResizeObserver(entries => {
             const target = entries[0].target;
-            navbarContainerRef.current.style.paddingRight = target.offsetWidth - target.clientWidth + "px";
+            navBar.style.paddingRight = target.offsetWidth - target.clientWidth + "px";
         });
-        outletObserver.observe(outletContainerRef.current);
 
-        return () => {
-            navObserver.disconnect();
-            outletObserver.disconnect();
-        };
+        navObserver.observe(navBar);
+        outletObserver.observe(outlet);
+
+        return () => { navObserver.disconnect(); outletObserver.disconnect(); };
     }, [navbarContainerRef.current, outletContainerRef.current]);
 
     return (
         <div className="flex flex-1 justify-between flex-col relative">
-            <Outlet />
+            <main ref={outletContainerRef} className="flex-1 flex flex-col overflow-y-auto">
+                <Outlet />
+            </main>
             <div
                 ref={navbarContainerRef}
                 className="absolute bottom-0 w-full text-white"
@@ -47,7 +46,7 @@ const Layout = () => {
                     <PageLink to="/Contact" >Contact</PageLink>
                 </nav>
             </div>
-        </div>
+        </div >
     );
 };
 
