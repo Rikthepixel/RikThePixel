896f86442eb33e77a07baa041672054acce1e5b6
RikThePixel <Rikdenbreejen@hotmail.nl>
2022-04-25 19:33:20 +0200

    [Change] Move over to Preact and make Vite work on gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,17 @@ import react from "@vitejs/plugin-react";
 import svgr from '@honkhonk/vite-plugin-svgr';
 
 export default defineConfig({
+    base: "/RikThePixel/",
     plugins: [
         react(),
         svgr()
-    ]
+    ],
+    resolve: {
+        alias: {
+            "react": "preact/compat",
+            "react-dom/test-utils": "preact/test-utils",
+            "react-dom": "preact/compat",
+            "react/jsx-runtime": "preact/jsx-runtime"
+        }
+    }
 });
